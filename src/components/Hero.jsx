62d8850fc47e5da041576468f584a8eaaf8acdf6
koonzinc <koonz.eth@gmail.com>
2022-08-26 12:59:38 -0400

    all

---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import heroVid from '../assets/video.mp4';
+
+const Hero = () => {
+  return (
+    <div className='w-full h-[90vh] top-[90px]'>
+        <video className='object-cover h-full w-full absolute -z-10' src={heroVid} autoPlay muted loop/>
+        <div className='w-full h-[90%] flex flex-col justify-center items-center text-white px-4 text-center'>
+            <h1>Decentralized</h1>
+            <h1 className='py-4'><span className='blue'>Trading</span> Protocol</h1>
+            <p className='text-xl py-4'>Guranteed liquidity trading for millions of users and top Ethereum applications.</p>
+            <div>
+                <button className='m-2 px-9'>Use Defi</button>
+                <button className='m-2 px-9'>FAQ</button>
+            </div>
+        </div>
+        <div>
+            <p className='text-center text-white text-2xl font-bold'>Total Volume Secured: $42,104,783,662.47</p>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
